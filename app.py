f989c617dc4a85abe3f9b7889ec3333ae96bf7ac
Rohan Kuntoji <rohan.kuntoji@gmail.com>
2019-10-04 16:50:40 +0530

    updated app.py with download endpoint
    The download endpoint works appropriately only on Safari browser. For Google Chrome, you will have to clear the cache manually everytime before uploading a file.

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, flash, request, redirect, render_template
-# from flask_restful import Api
-# from flask_httpauth import HTTPBasicAuth
+from flask import Flask, flash, request, redirect, render_template, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 from denoising import Denoising
@@ -11,13 +9,12 @@ sampled_noisy_speech_folder = os.path.join(path, 'datasets/sampled_noisy_speech'
 modfolder = os.path.join(path, 'models')
 denoise = Denoising(noisy_speech_folder=noisy_speech_folder, sampled_noisy_speech_folder=sampled_noisy_speech_folder, modfolder=modfolder)
 UPLOAD_FOLDER = noisy_speech_folder
+DOWNLOAD_FOLDER = os.path.join(path,'datasets/sampled_noisy_speech_denoised')
 
 app = Flask(__name__)
 app.secret_key = "speech denoising"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-# api = Api(app)
-# auth = HTTPBasicAuth()
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['wav']
@@ -32,12 +29,20 @@ def upload_form():
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash('File successfully uploaded')
-        return render_template('denoised_speech.html')
+        if allowed_file(file.filename):
+            filename = file.filename
+            filename = secure_filename(filename)
+            if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+                flash('File already exists. Please choose a different file')
+                return render_template('upload.html')
+            else:
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                flash('File successfully uploaded')
+                return render_template('denoised_speech.html')
+        else:
+            flash('Only wav files are supported currently')
+            return render_template('upload.html')
     else:
-        flash('Currently only wav file types are supported')
         return redirect(request.url)
 
 @app.route('/denoised_speech', methods=['GET', 'POST'])
@@ -46,6 +51,9 @@ def speech_denoising():
     denoise.inference()
     return render_template('success.html')
 
+@app.route('/download', methods=['GET'])
+def download_file():
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename=filename, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
